64bb580a89b84f805f457f68c11fc592dda4e380
Sergey Reus <reus.sergey14@gmail.com>
2017-10-24 22:04:10 +0300

    refactor(test/common) removed old code

--- test/common.js
+++ test/common.js
@@ -5,11 +5,6 @@ chai.use(require('sinon-chai'));
 
 global.loopback = require('loopback');
 
-// setup default data sources
-global.loopback.setDefaultDataSourceForType('db', {
-  connector: global.loopback.Memory
-});
-
 // auto attach data sources to models
 // global.loopback.autoAttach();
 chai.config.includeStack = true;
